91c8af46f11d9a920825d328770ae47d9f4696a2
Dionicio Diaz <dionicio@geniusplaza.com>
2020-08-11 09:03:32 -0400

    pass montd and year props to calendar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import moment from "moment";
+
 import './App.css';
 import Calendar from "components/Calendar";
 
 function App() {
+  const [currentMoth, setCurrentMoth] = React.useState(moment().month());
+  const [currentYear, setCurrentYear] = React.useState(moment().year());
+
   return (
     <div className="App">
      <h1 className="title"> Calendar Reminder App</h1>
-     <Calendar />
+     <Calendar  month={(currentMoth + 1)} year={currentYear} />
     </div>
   );
 }
