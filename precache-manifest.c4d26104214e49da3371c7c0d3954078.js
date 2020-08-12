a68812b8a4222f81cc72dd863f74a17356ce6b3b
Dionicio Diaz <dionicio@geniusplaza.com>
2020-08-11 23:10:54 -0400

    Updates

---
+++ precache-manifest.c4d26104214e49da3371c7c0d3954078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "facb85b1cc5464264233081752fb1e3a",
+    "url": "/calendar-reminder-app/index.html"
+  },
+  {
+    "revision": "53b353f6a30ec0dc54ea",
+    "url": "/calendar-reminder-app/static/css/main.e274a39f.chunk.css"
+  },
+  {
+    "revision": "6d619f44f6c7c1068a33",
+    "url": "/calendar-reminder-app/static/js/2.9574e53e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/calendar-reminder-app/static/js/2.9574e53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b353f6a30ec0dc54ea",
+    "url": "/calendar-reminder-app/static/js/main.5a390838.chunk.js"
+  },
+  {
+    "revision": "108a017062646d3740f6",
+    "url": "/calendar-reminder-app/static/js/runtime-main.f776d836.js"
+  }
+]);
