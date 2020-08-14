5a02033b4af6a8049039d4815cd6a096f2f06f17
Dionicio Diaz <dionicio@geniusplaza.com>
2020-08-13 21:23:09 -0400

    handle edit and create funtionality

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -8,7 +8,9 @@ import Form from "components/Form";
 import { CalendarWrapper, DayName } from "./styles";
 import Month from "./Month";
 import { selectMonth, selectYear} from "./Selectors";
-import { createReminder } from "./CalendarSlice";
+import { createReminder, updateReminder } from "components/Calendar/CalendarSlice";
+import { v4 as uuid } from "uuid";
+import EmptyReminder from 'constants/emptyReminder';
 
 const WeekDays = () => {
 	const daysName = moment.weekdays();
@@ -25,14 +27,29 @@ const WeekDays = () => {
 const Months = moment.months();
 
 const Calendar = ({}) => {
+  const dispatch = useDispatch();
   const month = useSelector(selectMonth);
   const year = useSelector(selectYear);
-
+  const [selectedReminder, setselectedReminder] = useState(EmptyReminder);
   const [Modal, openModal] = useModal(false);
 
-    const showModalReminder = React.useCallback(() =>{
+    const showModalReminder = React.useCallback((reminder) => {
+      setselectedReminder({...EmptyReminder,...reminder})
       openModal();
     },[]);
+
+    const submitHandler = (data)=> {
+      let reminder = {...data, id: uuid()};
+      if(selectedReminder.id){
+        reminder.id = selectedReminder.id;
+        dispatch(updateReminder(data));
+      }else{
+      let reminder = {...data, id: uuid()}
+        dispatch(createReminder(reminder));
+      }
+      setselectedReminder(EmptyReminder);
+    }
+
 	return (
     <>
     <h1 className="title">{Months[month-1]}</h1>
@@ -41,7 +58,10 @@ const Calendar = ({}) => {
       <Month currentMonth={month} currentYear={year} showModalReminder={showModalReminder} />
 		</CalendarWrapper>
       <Modal>
-          <Form></Form>
+          <Form
+            submitHandler={submitHandler}
+            initialData={selectedReminder}
+           />
       </Modal>
 </>
 	);
